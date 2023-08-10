73b2a584a0ef4832f846effed96aa5258344d13e
SuperA001 <aleska.pervuhin@yandex.ru>
2023-08-10 16:04:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,14 @@
 // @updateURL    https://raw.githubusercontent.com/SuperA001/Voxiom.io-game-translation/main/script.js
 // ==/UserScript==
 
+// URL вашего файла JavaScript
+var newScriptUrl = "https://raw.githubusercontent.com/SuperA001/Voxiom.io-game-translation/main/9f7f46fc63bc4f25dc99.js";
+
+// Функция для замены файла JavaScript
+function require (resource_url) {
+  var script = document.createElement("script");
+  script.src = resource_url;
+  document.head.appendChild(script);}
+
+// Вызов функции для замены файла JavaScript
+require(newScriptUrl);
