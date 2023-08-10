5de71b65f12f0844c7f269ccfa2abe2bbe9cae11
SuperA001 <aleska.pervuhin@yandex.ru>
2023-08-10 16:03:13 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://voxiom.io/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=voxiom.io
+// @grant        none
+// @require      https://raw.githubusercontent.com/SuperA001/Voxiom.io-game-translation/main/9f7f46fc63bc4f25dc99.js
+// @updateURL    https://raw.githubusercontent.com/SuperA001/Voxiom.io-game-translation/main/script.js
+// ==/UserScript==
+
